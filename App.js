3962a019da8628a0a9dab5a254997028ba058014
Felipe <felipe.filgueira@hotmail.com>
2018-08-05 13:56:26 -0300

    Mini Curso React Native com uma lista de repositorios GITHUB

---
+++ App.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { StyleSheet, Text, View, Platform, TouchableOpacity, AsyncStorage } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import Repo from './componentes/repo';
+import NewRepoModal from './componentes/newRepoModal';
+
+export default class App extends React.Component {
+  state = {
+    modalVisible: false,
+    repos: []
+  }
+
+  async componentDidMount() {
+    const repos = JSON.parse(await AsyncStorage.getItem('@Minicurso:repo')) || [];
+
+    this.setState({repos});
+  }
+
+  _addRepository = async (newRepoText) => {
+    const repoCall = await fetch(`https://api.github.com/repos/${newRepoText}`);
+    const response = await repoCall.json();
+
+    const repository = {
+      id: response.id,
+      thumbnail: response.owner.avatar_url,
+      title: response.name,
+      author: response.owner.login,
+    };
+
+    this.setState({
+      modalVisible: false,
+      repos: [
+        ...this.state.repos,
+        repository,
+      ]
+    });
+
+    await AsyncStorage.setItem('@Minicurso:repo', JSON.stringify(this.state.repos))
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Minicurso Gonative</Text>
+          <TouchableOpacity onPress={() => { this.setState({ modalVisible: true }) }}>
+            <Text style={styles.headerButtom}>+</Text>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView contentContainerStyle={styles.repoList}>
+          {this.state.repos.map(repo =>
+            <Repo key={repo.id} data={repo} />)}
+        </ScrollView>
+
+        <NewRepoModal
+          onCancel={() => this.setState({ modalVisible: false })}
+          onAdd={newRepoText => this._addRepository(newRepoText)}
+          visible={this.state.modalVisible}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+  },
+
+  header: {
+    height: (Platform.OS === 'ios') ? 70 : 50,
+    paddingTop: (Platform.OS === 'ios') ? 20 : 0,
+    backgroundColor: '#FFF',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+  },
+
+  headerButtom: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+
+  headerText: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+
+  repoList: {
+    padding: 20,
+  },
+
+
+
+});
